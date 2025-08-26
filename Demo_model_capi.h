1638666e6bc44015040cb365f01881bc27a4438d
Manojkannan0803 <63307410+Manojkannan0803@users.noreply.github.com>
2025-08-26 12:12:13 +0200

    Create Demo_model_capi.h

---
+++ Demo_model_capi.h
@@ -0,0 +1,32 @@
+
+Demo_model_ConstP
+/*
+ * File: Demo_model_capi.h
+ *
+ * Code generated for Simulink model 'Demo_model'.
+ *
+ * Model version                  : 1.4
+ * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+ * C/C++ source code generated on : Tue Aug 26 11:53:32 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef Demo_model_capi_h_
+#define Demo_model_capi_h_
+#include "Demo_model.h"
+
+extern void Demo_model_InitializeDataMapInfo(RT_MODEL_Demo_model_T *const
+  Demo_model_M);
+
+#endif                                 /* Demo_model_capi_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
+has popup
