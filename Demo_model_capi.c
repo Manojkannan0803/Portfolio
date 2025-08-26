546b73383580e9d8305d190a9a9fe0b811247d10
Manojkannan0803 <63307410+Manojkannan0803@users.noreply.github.com>
2025-08-26 12:11:56 +0200

    Create Demo_model_capi.c

---
+++ Demo_model_capi.c
@@ -0,0 +1,74 @@
+
+Demo_model_ConstP
+/*
+ * File: Demo_model_capi.c
+ *
+ * Code generated for Simulink model 'Demo_model'.
+ *
+ * Model version                  : 1.4
+ * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+ * C/C++ source code generated on : Tue Aug 26 11:53:32 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include <stddef.h>
+#include "rtw_capi.h"
+#ifdef HOST_CAPI_BUILD
+#include "Demo_model_capi_host.h"
+#define sizeof(...)                    ((size_t)(0xFFFF))
+#undef rt_offsetof
+#define rt_offsetof(s,el)              ((uint16_T)(0xFFFF))
+#define TARGET_CONST
+#define TARGET_STRING(s)               (s)
+#else                                  /* HOST_CAPI_BUILD */
+#include "builtin_typeid_types.h"
+#include "Demo_model.h"
+#include "Demo_model_capi.h"
+#ifdef LIGHT_WEIGHT_CAPI
+#define TARGET_CONST
+#define TARGET_STRING(s)               ((NULL))
+#else
+#define TARGET_CONST                   const
+#define TARGET_STRING(s)               (s)
+#endif
+#endif                                 /* HOST_CAPI_BUILD */
+
+/* Individual block tuning is not valid when inline parameters is *
+ * selected. An empty map is produced to provide a consistent     *
+ * interface independent  of inlining parameters.                 *
+ */
+static rtwCAPI_BlockParameters rtBlockParameters[] = {
+  /* addrMapIndex, blockPath,
+   * paramName, dataTypeIndex, dimIndex, fixPtIdx
+   */
+  {
+    0, (NULL), (NULL), 0, 0, 0
+  }
+};
+
+/* Tunable variable parameters */
+static rtwCAPI_ModelParameters rtModelParameters[] = {
+  /* addrMapIndex, varName, dataTypeIndex, dimIndex, fixPtIndex */
+  { 0, (NULL), 0, 0, 0 }
+};
+
+/* Data Type Map - use dataTypeMapIndex to access this structure */
+static TARGET_CONST rtwCAPI_DataTypeMap rtDataTypeMap[] = {
+  /* cName, mwName, numElements, elemMapIndex, dataSize, slDataId, *
+   * isComplex, isPointer, enumStorageType */
+  {
+    "", "", 0, 0, 0, 0, 0, 0, 0
+  }
+};
+
+#ifdef HOST_CAPI_BUILD
+#undef sizeof
+#endif
+
+/* Structure Element Map - use elemMapIndex to access this structure */
+static TARGET_CONST rtwCAPI_ElementMap rtElementMap[] = {
+has popup
