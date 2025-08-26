2d389a4d61e48f045cdb80f78a1fe6c4e481dbdb
Manojkannan0803 <63307410+Manojkannan0803@users.noreply.github.com>
2025-08-26 12:10:57 +0200

    Create Demo_model_data.c

---
+++ Demo_model_data.c
@@ -0,0 +1,36 @@
+
+Demo_model_ConstP
+/*
+ * File: Demo_model_data.c
+ *
+ * Code generated for Simulink model 'Demo_model'.
+ *
+ * Model version                  : 1.4
+ * Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
+ * C/C++ source code generated on : Tue Aug 26 11:53:32 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "Demo_model.h"
+
+/* Constant parameters (default storage) */
+const ConstP_Demo_model_T Demo_model_ConstP = {
+  /* Expression: Demo.getconfig
+   * Referenced by: '<Root>/Demo_setpoint_adjuster'
+   */
+  {
+    10.0,
+    1.0e-13
+  }
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
+has popup
